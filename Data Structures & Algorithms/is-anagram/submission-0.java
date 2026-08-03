4b78f234fd7c5ff06d65c121b4756c1002afb1a2
virus8vedant <virus8vedant@proton.me>
2026-08-03 19:50:38 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+        public boolean isAnagram(String s, String t) {
+
+            char[] arr1 = s.toCharArray();
+            char[] arr2 = t.toCharArray();
+
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+
+            return Arrays.toString(arr1).equals(Arrays.toString(arr2));
+        }
+}
